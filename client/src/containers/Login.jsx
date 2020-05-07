28d043febbc2d36ef505c474df878ef6d4fb4a35
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-05-07 10:00:08 -0400

    abstracted form from login

--- client/src/containers/Login.jsx
+++ client/src/containers/Login.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Input from "../components/Shared/Input/Input";
+import Form from "../components/Shared/Form/Form";
 
 class Login extends Component {
   state = {
@@ -16,6 +17,10 @@ class Login extends Component {
       error: "",
     });
   };
+  
+  // TODO: Write two separate submit functions
+  // 1. Calls your new user route.
+  // 2. Calls your existing user login route. 
 
   handleSubmit = (event) => {
     event.preventDefault();
@@ -37,77 +42,7 @@ class Login extends Component {
   };
   render() {
     return (
-      <div className="container">
-        {this.state.error && (
-          <div
-            className="row"
-            id="login-alert"
-            style={{ backgroundColor: "#FE6D73", paddingTop: 3 }}
-          >
-            <div className="col">
-              <p style={{ color: "#ffffff" }}>{this.state.error}</p>
-            </div>
-          </div>
-        )}
-        <div className="row">
-          <form className="col s12" onSubmit={this.handleSubmit}>
-            <div className="row">
-              {/* <div className="input-field col s12">
-                <input
-                  id="email"
-                  type="email"
-                  className="validate"
-                  name="email"
-                  value={this.state.email}
-                  onChange={this.handleInputChange}
-                />
-                <label htmlFor="email">Email</label>
-              </div> */}
-              <Input
-                id="email"
-                type="email"
-                name="email"
-                value={this.state.email}
-                label="Email"
-                handleChange={this.handleInputChange}
-              />
-            </div>
-            <div className="row">
-              {/* <div className="input-field col s12">
-                <input
-                  id="password"
-                  type="password"
-                  className="validate"
-                  name="password"
-                  value={this.state.password}
-                  onChange={this.handleInputChange}
-                />
-                <label htmlFor="password">Password</label>
-              </div> */}
-              <Input
-                id="password"
-                type="password"
-                name="password"
-                value={this.state.password}
-                label="Password"
-                handleChange={this.handleInputChange}
-              />
-            </div>
-            <div className="row">
-              <div className="col">
-                <button
-                  className="btn waves-effect waves-light"
-                  type="submit"
-                  name="action"
-                  style={{ backgroundColor: "#227C9D" }}
-                >
-                  Submit
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
+      <Form handleSubmit={this.handleSubmit} error={this.state.error}/>
     );
   }
 }
